9f32c3f59000e7c28adfe8182c9423781366fd72
Emil <somaleyoo123@gmail.com>
2025-05-26 14:22:55 +0200

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+
 
 function App() {
  
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from '../Components/Nav'
+import './home.css'
+
+export default function  Home(props) {
+    
+
+    return(
+        <>
+            <Nav/>
+        </>
+    )
+}
