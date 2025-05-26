0eb90a5614c45b7dc5b948f6a2fc632d8b741f7a
Emil <somaleyoo123@gmail.com>
2025-05-26 14:53:02 +0200

    json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
@@ -10,9 +10,9 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
     </>
   )
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './nav.css'
 
 export default function Nav(props) {
@@ -6,30 +7,45 @@ export default function Nav(props) {
 
     return(
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+           <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      JoJo
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            About
+          </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Pricing
+          </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+       
       </ul>
     </div>
   </div>
 </nav>
+
         </>
     )
 }
